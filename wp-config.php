5ceb50ccc46af37bfabd528c6fdaf21c31d7e1f3
Joshua Muinde <MuindeJoshua8@gmail.com>
2023-10-24 14:01:17 +0300

    wordpress Rehearsals begin

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,9 @@ define( 'DB_CHARSET', 'utf8mb4' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+/** to enable me to play with plugins and updates without FTP/SFTP interuptions */
+define('FS_METHOD','direct');
+
 /**#@+
  * Authentication unique keys and salts.
  *
--- wp-content/themes/bitlink/single.php
+++ wp-content/themes/bitlink/single.php
@@ -6,8 +6,14 @@
     <title>Document</title>
 </head>
 <body>
-
-This is HOME
-
+<?php
+while (have_posts()){
+    the_post();?>
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+    <hr>
+    <?php
+}
+?>
 </body>
 </html>
